20970937b94eba26c0a5db9daddc89dce1a2d274
Sonu jatav <sonuj22661@gmail.com>
2025-03-22 18:12:28 +0530

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  bese:"/portfolio-website/",
+  bese:"/profile-web/",
   plugins: [tailwindcss(),react()],
 })
